52a6ef7282c3ca161cdac6a484105d027628cef2
Jan Lana <jan.lana@gyarab.cz>
2024-09-13 10:35:42 +0200

    initial structure

--- src/main/java/cz/gyarab/e2prg/s1/Main.java
+++ src/main/java/cz/gyarab/e2prg/s1/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package cz.gyarab.e2prg.s1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
