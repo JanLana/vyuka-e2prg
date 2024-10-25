73c085ed1f0867d015cee70fe18a1eac7a054084
Jan Lana <jan.lana@gyarab.cz>
2024-10-25 12:17:32 +0200

    dnesni hodina - s2

--- src/main/java/cz/gyarab/e2prg/s2/Clovek.java
+++ src/main/java/cz/gyarab/e2prg/s2/Clovek.java
@@ -1,5 +1,7 @@
 package cz.gyarab.e2prg.s2;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.Scanner;
 
 public class Clovek {
@@ -33,20 +35,54 @@ public class Clovek {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Clovek[] arr = new Clovek[5];
+        Clovek[] arr = inicializceRandom(); //new Clovek[5];
+        bubbleSort(arr);
+        //selectSort(arr);
+        vypis(arr);
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            String jmeno = sc.next();
-            String prijmeni = sc.next();
-            arr[i] = new Clovek(jmeno, prijmeni);
-        }
-
+    private static void vypis(Clovek[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        System.out.println("=========");
+    }
 
+    private static Clovek[] inicializceRandom() {
+        Clovek[] arr = new Clovek[100];
+        for (int i = 0; i < arr.length; i++) {
+            String jmeno = RandomStringUtils.random(10, true, false);
+            String prijmeni = RandomStringUtils.random(10, true, true);
+            arr[i] = new Clovek(jmeno, prijmeni);
+        }
+        return arr;
+    }
+
+
+        private static Clovek[] inicializce() {
+        return new Clovek[] {
+                new Clovek("a", "A"),
+                new Clovek("e", "E"),
+                new Clovek("f", "F"),
+                new Clovek("b", "B"),
+                new Clovek("x", "X"),
+                new Clovek("c", "C")
+        };
+    }
+
+    private static void selectSort(Clovek[] arr) {
+        for (int kolikJeSetrideno = 0; kolikJeSetrideno < arr.length; kolikJeSetrideno++) {
+            int poziceNejmensihoPrvku = kolikJeSetrideno;
+            // posun poziceNP na skutecne nejmesi
+            Clovek nejmensi = arr[poziceNejmensihoPrvku];
+            arr[poziceNejmensihoPrvku] = arr[kolikJeSetrideno];
+            arr[kolikJeSetrideno] = nejmensi;
+
+        }
+
+    }
+
+
+    private static void bubbleSort(Clovek[] arr) {
         int pocetProhozeni = 1;
 
         while (pocetProhozeni > 0) {
@@ -61,11 +97,6 @@ public class Clovek {
                     pocetProhozeni += 1;
                 }
             }
-        }
-
-        // vypis
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
         }
     }
 }
