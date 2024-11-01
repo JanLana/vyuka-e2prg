4446d50b4ca30753e75ba5aea1be108c35b63a93
Jan Lana <jan.lana@gyarab.cz>
2024-11-01 10:35:29 +0100

    dnesni hodina - s1

---
+++ src/main/java/cz/gyarab/e2prg/s1/TrideniSlevanim.java
@@ -0,0 +1,67 @@
+package cz.gyarab.e2prg.s1;
+
+import java.util.Arrays;
+
+public class TrideniSlevanim {
+    public static void main(String[] args) {
+        int[] arr = { 5,2,13,1,8,7,3,4 };
+
+        int[] vysledek = mergeSort(arr);
+
+        Arrays.toString(vysledek);
+    }
+
+    public static int[] mergeSort(int[] vstup) {
+        if (vstup.length == 1) {
+            return vstup;
+        }
+        int pul = vstup.length / 2;
+        int[] leva = new int[pul];
+        int[] prava = new int[pul];
+        // 1/2 vstup -> leva, 2/2 -> prava;
+
+        for (int i = 0; i < pul; i++) {
+            leva[i] = vstup[i];
+        }
+
+
+        for (int i = pul; i < vstup.length; i++) {
+            prava[i-pul] = vstup[i];
+        }
+
+       // System.out.println("vstup: " + Arrays.toString(vstup));
+        //System.out.println("leva " + Arrays.toString(leva));
+        //System.out.println("prava: " + Arrays.toString(prava));
+
+        int[] levaSetridena = mergeSort(leva);
+        int[] pravaSetridena = mergeSort(prava);
+
+        int[] vysledek = merge(leva, prava);
+
+        return vysledek;
+    }
+
+    public static int[] merge(int[] leva, int[] prava) {
+        int i = 0, j = 0, k = 0;
+        int[] vysledek = new int[leva.length + prava.length];
+
+        while (i < leva.length && j < prava.length) {
+            System.out.println("i=" + i + " j=" + j + " k=" + k);
+            System.out.println("vstup: " + Arrays.toString(vysledek));
+            System.out.println("leva " + Arrays.toString(leva));
+            System.out.println("prava: " + Arrays.toString(prava));
+
+            if (leva[i] <= prava[j]) {
+                vysledek[k] = leva[i];
+                k += 1;
+                i += 1;
+            } else {
+                vysledek[k] = prava[j];
+                k += 1;
+                j += 1;
+            }
+        }
+
+        return vysledek;
+    }
+}
